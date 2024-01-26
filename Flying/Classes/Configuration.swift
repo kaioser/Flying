442125f01506b9d126fc815fc727946fdfca1e53
yangxiongkai <yangxiongkai@126.com>
2024-01-26 10:11:03 +0800

    nothing

--- Flying/Classes/Configuration.swift
+++ Flying/Classes/Configuration.swift
@@ -12,28 +12,28 @@ let kFlyConfiguration = "FlyConfiguration"
 
 public class FlyConfiguration {
     
-    static var plugins: [PluginType] = []
+    private static var plugins: [PluginType] = []
     
-//    public static func register(environments: [Environment], plugins: [PluginType]) {
-//
-//        UserDefaultsManager.save(environments: environments)
-//        self.plugins = plugins
-//    }
-//
-//    static var current: String? = UserDefaultsManager.fetch().filter { $0.isInitial }.first?.baseURL
+    public static func register(environments: [Environment], plugins: [PluginType]) {
+
+        UserDefaultsManager.save(environments: environments)
+        self.plugins = plugins
+    }
+
+    static var current: String? = UserDefaultsManager.fetch().filter { $0.isInitial }.first?.baseURL
 }
 
-//public protocol Environment {
-//    var baseURL: String { get }
-//    var isInitial: Bool { get }
-//}
+public protocol Environment {
+    var baseURL: String { get }
+    var isInitial: Bool { get }
+}
 
-//public extension TargetType {
-//
-//    var baseURL: URL {
-//        if let urlString = FlyConfiguration.current {
-//            return URL(string: urlString)!
-//        }
-//        return URL(string: "https://github.com")!
-//    }
-//}
+public extension TargetType {
+
+    var baseURL: URL {
+        if let urlString = FlyConfiguration.current {
+            return URL(string: urlString)!
+        }
+        return URL(string: "https://github.com")!
+    }
+}
